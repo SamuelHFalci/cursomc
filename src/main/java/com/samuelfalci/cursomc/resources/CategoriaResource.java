de1d9e609054efaa753efce560cc2883fc0e087c
Samuka <Samuka@DESKTOP-KJQA4DD>
2018-11-07 11:16:26 -0200

    Inserido uma nova Categoria com POST

--- src/main/java/com/samuelfalci/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/samuelfalci/cursomc/resources/CategoriaResource.java
@@ -2,14 +2,18 @@ package com.samuelfalci.cursomc.resources;
 
 import com.samuelfalci.cursomc.domain.Categoria;
 import com.samuelfalci.cursomc.services.CategoriaService;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -23,5 +27,12 @@ public class CategoriaResource {
 
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/samuelfalci/cursomc/services/CategoriaService.java
+++ src/main/java/com/samuelfalci/cursomc/services/CategoriaService.java
@@ -18,4 +18,9 @@ public class CategoriaService {
         }
         return obj;
     }
+    
+    public Categoria insert(Categoria obj){
+        obj.setId(null);
+        return repo.save(obj);
+    }
 }
