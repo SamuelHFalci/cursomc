4375c460fa13e2f8eb0c270bb0a3761a4dc3906d
Samuka <Samuka@DESKTOP-KJQA4DD>
2018-11-07 13:38:24 -0200

    Put Delete e Get para Cliente

---
+++ src/main/java/com/samuelfalci/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samuelfalci.cursomc.dto;
+
+import com.samuelfalci.cursomc.domain.Cliente;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ *
+ * @author Samuka
+ */
+public class ClienteDTO {
+
+    private Integer id;
+    @NotEmpty(message = "Preenchimento Obrigatorio")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+    private String nome;
+    @NotEmpty(message = "Preenchimento Obrigatorio")
+    @Email(message = "Email Inválido")
+    private String email;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente obj) {
+        id = obj.getId();
+        nome = obj.getNome();
+        email = obj.getEmail();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
--- src/main/java/com/samuelfalci/cursomc/services/ClienteService.java
+++ src/main/java/com/samuelfalci/cursomc/services/ClienteService.java
@@ -1,21 +1,63 @@
 package com.samuelfalci.cursomc.services;
 
 import com.samuelfalci.cursomc.domain.Cliente;
+import com.samuelfalci.cursomc.domain.enums.TipoCliente;
+import com.samuelfalci.cursomc.dto.ClienteDTO;
 import com.samuelfalci.cursomc.repositories.ClienteRepository;
+import com.samuelfalci.cursomc.services.exceptions.DataIntegrityException;
 import com.samuelfalci.cursomc.services.exceptions.ObjectNotFoundException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
+
     @Autowired
     private ClienteRepository repo;
-    
-    public Cliente find(Integer id){
+
+    public Cliente find(Integer id) {
         Cliente obj = repo.findOne(id);
-        if(obj == null){
-            throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id +", Tipo: "+Cliente.class.getName());
+        if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
         }
         return obj;
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.delete(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possivel excluir porque ha entidades relacionadas");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return repo.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto) {
+        return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
     }
 }
