4007fc80ab1e163acd1445e70a5def8f7b46231a
Samuka <Samuka@DESKTOP-KJQA4DD.local>
2018-11-13 11:19:07 -0200

    Validação customizada de CPF ou CNJP na inserção de Cliente

--- src/main/java/com/samuelfalci/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/samuelfalci/cursomc/dto/ClienteNewDTO.java
@@ -5,30 +5,37 @@
  */
 package com.samuelfalci.cursomc.dto;
 
+import com.samuelfalci.cursomc.services.validation.ClienteInsert;
 import java.io.Serializable;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
 
-/**
- *
- * @author Samuka
- */
-public class ClienteNewDTO implements Serializable{
-    
+@ClienteInsert
+public class ClienteNewDTO implements Serializable {
+
+    @NotEmpty(message = "Preenchimento Obrigatorio")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
+    @NotEmpty(message = "Preenchimento Obrigatorio")
+    @Email(message = "Email Inválido")
     private String email;
+    @NotEmpty(message = "Preenchimento Obrigatorio")
     private String cpfOuCnpj;
     private Integer tipo;
-    
-    
+    @NotEmpty(message = "Preenchimento Obrigatorio")
     private String logradouro;
+    @NotEmpty(message = "Preenchimento Obrigatorio")
     private String numero;
     private String complemento;
     private String bairro;
+    @NotEmpty(message = "Preenchimento Obrigatorio")
     private String cep;
-    
+    @NotEmpty(message = "Preenchimento Obrigatorio")
     private String telefone1;
     private String telefone2;
     private String telefone3;
-    
+
     private Integer cidadeId;
 
     public ClienteNewDTO() {
@@ -137,6 +144,5 @@ public class ClienteNewDTO implements Serializable{
     public void setCidadeId(Integer cidadeId) {
         this.cidadeId = cidadeId;
     }
-    
-    
+
 }
---
+++ src/main/java/com/samuelfalci/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samuelfalci.cursomc.services.validation;
+
+import com.samuelfalci.cursomc.domain.enums.TipoCliente;
+import com.samuelfalci.cursomc.dto.ClienteNewDTO;
+import com.samuelfalci.cursomc.resources.exception.FieldMessage;
+import com.samuelfalci.cursomc.services.validation.utils.BR;
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+        
+        if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
+            list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+        }
+        if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
+            list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+        }
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
