800db4cf374cd8cd2265fde73bcb5d52bd201ad3
Samuka <Samuka@DESKTOP-KJQA4DD.local>
2018-11-13 11:37:32 -0200

    Validação customizada: Email nao repetido na insercao de clientes

--- src/main/java/com/samuelfalci/cursomc/domain/Cliente.java
+++ src/main/java/com/samuelfalci/cursomc/domain/Cliente.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,6 +24,7 @@ public class Cliente implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private String cpfOuCnpj;
     private Integer tipo;
--- src/main/java/com/samuelfalci/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/samuelfalci/cursomc/services/validation/ClienteInsertValidator.java
@@ -5,17 +5,23 @@
  */
 package com.samuelfalci.cursomc.services.validation;
 
+import com.samuelfalci.cursomc.domain.Cliente;
 import com.samuelfalci.cursomc.domain.enums.TipoCliente;
 import com.samuelfalci.cursomc.dto.ClienteNewDTO;
+import com.samuelfalci.cursomc.repositories.ClienteRepository;
 import com.samuelfalci.cursomc.resources.exception.FieldMessage;
 import com.samuelfalci.cursomc.services.validation.utils.BR;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+    @Autowired
+    private ClienteRepository repo;
+    
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -29,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         }
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+        }
+        
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email", "Email já existente"));
         }
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
